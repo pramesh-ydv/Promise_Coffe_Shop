484e5711a8bbe71007c78b320f96c516b9c145bd
pramesh-ydv <prameshkyadav2020@gmail.com>
2024-12-19 20:18:39 +0545

    Files Added

--- android/app/src/main/java/com/promise_coffe_shop/MainActivity.kt
+++ android/app/src/main/java/com/promise_coffe_shop/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.promise_coffe_shop
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,7 +12,14 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Promise_Coffe_Shop"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)  // Ensure this line is correct
+    }
+
+    override fun getMainComponentName(): String {
+        return "Promise_Coffe_Shop"
+    }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
